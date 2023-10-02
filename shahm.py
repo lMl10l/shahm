44bf5ee6db31c64137ac26346cd76006cb282b02
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-10-02 21:37:51 +0300

    Update shahm.py

--- shahm.py
+++ shahm.py
@@ -15,7 +15,7 @@ user_env_vars = {}
 TELEGRAM_TOKEN = '5970239537:AAF8OqpJ8kZMNXyZcfnCuJwQ0ZalW_KZ4DA'
 HEROKU_APP_NAME = 'aljokeruaerbot'
 
-updater = Updater(TELEGRAM_TOKEN, use_context=True)
+updater = Updater(token=TELEGRAM_TOKEN)
 bot = Bot(token=TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
 
