f0883e6efb0c327b2c7408c70d1695aed9782f02
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-10-02 14:57:17 +0300

    Update shahm.py

--- shahm.py
+++ shahm.py
@@ -4,7 +4,7 @@ import logging
 api_id = '12706503'
 api_hash = '4344a75f9ae9d1d03f577b61e7313884'
 bot_token = '5970239537:AAF8OqpJ8kZMNXyZcfnCuJwQ0ZalW_KZ4DA'
-bot_owner_id = '5564802580'
+owner_id = '5564802580'
 
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
@@ -12,22 +12,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-@client.on(events.NewMessage)
-async def forward_message(event):
-    if event.text:
-        sender = await event.get_sender()
-        sender_id = sender.id
-        if sender_id == 5564802580:
-            await event.forward_to(sender.id)
+@client.on(events.NewMessage(incoming=True))
+async def handle_new_message(event):
+    if event.from_id == owner_id:
+        return
+    await client.forward_messages(owner_id, event.message)
 
-@client.on(events.NewMessage(pattern='/start'))
-async def start(event):
-    await event.respond('مرحبًا بك! أنا بوت تليجرام.')
-
-@client.on(events.NewMessage(outgoing=True))
-async def send_reply(event):
-    if event.text:
-        await event.respond(event.text)
-
-with client:
-    client.run_until_disconnected()
+if __name__ == '__main__':
+    with client:
+        client.run_until_disconnected()
