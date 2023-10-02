a2a4560faac6eba663fdaf035d1ef988373c0621
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-10-02 14:07:53 +0300

    Update shahm.py

--- shahm.py
+++ shahm.py
@@ -1,14 +1,16 @@
 from telethon.sync import TelegramClient, events
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 api_id = '12706503'
 api_hash = '4344a75f9ae9d1d03f577b61e7313884'
 bot_token = '5970239537:AAF8OqpJ8kZMNXyZcfnCuJwQ0ZalW_KZ4DA'
 
+logging.basicConfig(level=logging.INFO)
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-print("البوت قيد التشغيل...")
+
+logger = logging.getLogger(__name__)
+logger.info("البوت قيد التشغيل...")
+
 
 @client.on(events.NewMessage)
 async def forward_message(event):
