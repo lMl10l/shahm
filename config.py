9837b9785690c996aff4d4cb6e3f1d45b65da285
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-10-03 03:43:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-api_id = os.environ.get("api_hash")
+api_id = os.environ.get("api_id")
 api_hash = os.environ.get("api_hash")
 bot_token = os.environ.get("bot_token")
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
