ca5a1f7cd8e4f2ee9fb5cd885d21e2b81431d31a
جيبثون العربي <96865194+lml10l@users.noreply.github.com>
2023-11-13 23:13:52 +0300

    Update shahm.py

--- shahm.py
+++ shahm.py
@@ -1,96 +1,56 @@
-import schedule
-import time
-import pickle
-import random
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler
-from telegram.ext.filters import Filters
-from telegram.ext import InlineKeyboardButton, InlineKeyboardMarkup
+from telethon import TelegramClient, events
+from telethon.tl.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+api_id = '5116461'
+api_hash = 'e9fe4b672f162a27f78d8af54b004e25'
+YOUR_OWNER_ID = '705475246'
 bot_token = '6330369637:AAGAnd4SsT-WmXgFYU8Zy5q0Dnzw4p7Ow8s'
-bot_owner_id = '705475246'
 
-bot = Bot(token=bot_token)
+client = TelegramClient('session_name', api_id, api_hash)
+
 adhkar_list = []
 enabled_groups = set()
-interval_minutes = 15
-
-def send_adhkar():
+set_interval = 15
+async def send_adhkar():
     if adhkar_list:
         random_adhkar = random.choice(adhkar_list)
         for chat_id in enabled_groups:
-            bot.send_message(chat_id=chat_id, text=f"*{random_adhkar}*", parse_mode="Markdown")
-def set_interval(update, context):
-    user_id = str(update.message.from_user.id)
-    if user_id != bot_owner_id:
-        context.bot.send_message(chat_id=update.message.chat_id, text="ليس لديك الصلاحية لتغيير فاصل الزمني.")
-        return
-    context.bot.send_message(chat_id=update.message.chat_id, text="أدخل عدد الدقائق لإرسال الأذكار:")
-    return "WAIT_INTERVAL"
+            await client.send_message(chat_id, f"*{random_adhkar}*", parse_mode="Markdown")
 
-def receive_interval(update, context):
-    try:
-        global interval_minutes
-        interval_minutes = int(update.message.text)
-        context.bot.send_message(chat_id=update.message.chat_id, text=f"تم تعيين فاصل زمني للإرسال كل {interval_minutes} دقيقة.")
-        schedule.clear()
-        schedule.every(interval_minutes).minutes.do(send_adhkar)
-        return
-    except ValueError:
-        context.bot.send_message(chat_id=update.message.chat_id, text="الرجاء إدخال عدد صحيح.")
-
-def start(update, context):
-    user_id = str(update.message.from_user.id)
-    if user_id == bot_owner_id:
-        keyboard = [
-            [InlineKeyboardButton("إضافة أذكار", callback_data='add_adhkar')],
-            [InlineKeyboardButton("تعيين فاصل زمني", callback_data='set_interval')]
+@client.on(events.NewMessage(pattern='/start'))
+async def start(event):
+    user_id = event.from_id
+    if user_id == YOUR_OWNER_ID:
+        buttons = [
+            [InlineKeyboardButton("إضافة أذكار", b'add_adhkar')],
+            [InlineKeyboardButton("تعيين فاصل زمني", b'set_interval')]
         ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text('مرحبًا! اضغط على الزر لإضافة الأذكار أو تعيين فاصل زمني:', reply_markup=reply_markup)
+        keyboard = InlineKeyboardMarkup(buttons)
+        await event.respond('مرحبًا! اضغط على الزر لإضافة الأذكار أو تعيين فاصل زمني:', reply_markup=keyboard)
     else:
-        update.message.reply_text('أنت لا تمتلك الصلاحية لاستخدام هذه الأوامر.')
+        await event.respond('مرحبًا! لا يمكنك استخدام هذه الأوامر.')
 
-def button_click(update, context):
-    query = update.callback_query
-    query.answer()
+@client.on(events.CallbackQuery)
+async def button_click(event):
+    if event.data == b'add_adhkar':
+        await event.respond("أرسل لي الأذكار التي تريد إضافتها.")
+    elif event.data == b'set_interval':
+        user_id = event.from_id
+        if user_id == YOUR_OWNER_ID:
+            await event.respond("أدخل عدد الدقائق لإرسال الأذكار:")
+        else:
+            await event.respond("ليس لديك الصلاحية لتعيين الفاصل الزمني.")
 
-    if query.data == 'add_adhkar':
-        context.bot.send_message(chat_id=query.message.chat_id, text="أرسل لي الأذكار التي تريد إضافتها.")
-    elif query.data == 'set_interval':
-        set_interval(update, context)
+@client.on(events.NewMessage(pattern='/enable'))
+async def enable_group(event):
+    chat_id = event.chat_id
+    enabled_groups.add(chat_id)
+    await event.respond("تم تفعيل الإرسال لهذه المجموعة!")
 
-def add_adhkar(update, context):
-    user_id = str(update.message.from_user.id)
-    if user_id == bot_owner_id:
-        adhkar_text = update.message.text
-        adhkar_list.append(adhkar_text)
-        context.bot.send_message(chat_id=update.message.chat_id, text="تمت إضافة الأذكار بنجاح!")
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="ليس لديك الصلاحية لإضافة أذكار.")
+async def add_adhkar_function(user_id, adhkar_text):
+async def set_interval_function(user_id, minutes):
 
-def enable_group(update, context):
-    user_id = str(update.message.from_user.id)
-    if user_id == bot_owner_id:
-        chat_id = update.message.chat_id
-        enabled_groups.add(chat_id)
-        context.bot.send_message(chat_id=chat_id, text="تم تفعيل الإرسال لهذه المجموعة!")
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="ليس لديك الصلاحية لتفعيل الإرسال للمجموعات.")
+schedule.every(15).minutes.do(send_adhkar)
 
-schedule.every(interval_minutes).minutes.do(send_adhkar)
-
-updater = Updater(token=bot_token, use_context=True)
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CallbackQueryHandler(button_click))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, add_adhkar))
-dispatcher.add_handler(CommandHandler('تفعيل', enable_group))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, receive_interval))
-
-updater.start_polling()
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+client.start()
+client.run_until_disconnected()
